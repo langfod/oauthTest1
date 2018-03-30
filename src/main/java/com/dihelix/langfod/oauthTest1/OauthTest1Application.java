f02f446fb0a92f64865476104e227b3fa34b5919
David Langford <langfod@gmail.com>
2018-03-30 14:26:34 -0400

    first commit

---
+++ src/main/java/com/dihelix/langfod/oauthTest1/OauthTest1Application.java
@@ -0,0 +1,12 @@
+package com.dihelix.langfod.oauthTest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthTest1Application.class, args);
+	}
+}
